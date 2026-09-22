82cf15cfdd3dcf8599ab68bae1642685f89e650d
lacheski <suportemundoconhecimento@gmail.com>
2026-09-22 16:02:49 -0300

    corrigindo bug

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.querySelectorAll('.promo-date').forEach(el => {
-    const today = new Date();
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0');
-    const yyyy = today.getFullYear();
-    el.textContent = `${dd}/${mm}/${yyyy}`;
-});
-
 function redirectWithParams(destination) {
     const currentParams = window.location.search;
     if (!currentParams) {
